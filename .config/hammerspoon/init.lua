f52b9c2563a7c5a6c41897f96f4e21d2c8728057
jshu <jasonshu@terpmail.umd.edu>
2026-07-06 12:39:49 -0400

    hammerspoon

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,13 +1,13 @@
 -- https://github.com/Hammerspoon/hammerspoon/pull/582
 -- defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 
-hs.loadSpoon("EmmyLua")
-
 -- application switcher
 for key, name in pairs({
 	["1"] = "Google Chrome",
-	["2"] = "Ghostty",
-	["3"] = "Notes",
+	["2"] = "Visual Studio Code",
+	["3"] = "Ghostty",
+	-- ["3"] = "Notes",
+	-- ["e"] = "Claude"
 }) do
 	hs.hotkey.bind("alt", key, function()
 		hs.application.launchOrFocus(name)
