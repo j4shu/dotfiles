f01b9375455e1ab83e902e8636e164a9c839beba
jshu <jasonshu@terpmail.umd.edu>
2026-07-08 14:58:38 -0400

    surround

--- .config/nvim/lua/plugins/operators/mini-surround.lua
+++ .config/nvim/lua/plugins/operators/mini-surround.lua
@@ -8,21 +8,7 @@ require('mini.surround').setup({
     -- lua function
     f = { output = { left = 'function() ', right = ' end' } },
   },
-  mappings = {
-    add = 'sS',
-    -- delete = 'sd',
-    -- find = 'sf',
-    -- find_left = 'sF',
-    -- highlight = 'sh',
-    -- replace = 'sr',
-
-    update_n_lines = '',
-    suffix_last = '',
-    suffix_next = '',
-  },
   n_lines = 200,
   respect_selection_type = true,
 })
-
 vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
-vim.keymap.set('n', 'sA', 'sS_', { remap = true, desc = 'Add surrounding (New Lines)' })
