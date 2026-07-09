594350e986d45fa9b36f8ba2c4549a6bffae78ea
jshu <jasonshu@terpmail.umd.edu>
2026-07-09 18:51:16 -0400

    autocommands

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -5,6 +5,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- disable auto comments
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function(args)
+    -- Defer past the built-in ftplugin, which re-adds r/o via `formatoptions+=croql`.
+    vim.schedule(function()
+      if vim.api.nvim_buf_is_valid(args.buf) then
+        vim.bo[args.buf].formatoptions = vim.bo[args.buf].formatoptions:gsub('[ro]', '')
+      end
+    end)
+  end,
+})
+
 -- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
   callback = function()
